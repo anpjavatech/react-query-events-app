1e9cd6c3f7022d7df9ac909a297a547c1d5071a3
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-04-14 20:30:30 +0200

    Commit after adding query mutation logic from tanstack libs.

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,85 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { fetchEvent, updateEvent } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from "../../util/http.js";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+  const { mutate, isPending: isMutationPending } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) =>{
+      const newEvent = data.event;
+      await queryClient.cancelQueries({queryKey:["events", params.id]});
+      const prevEvent = queryClient.getQueryData(["events", params.id]);
+      queryClient.setQueryData(["events", params.id], newEvent);
 
-  function handleSubmit(formData) {}
+      return {prevEvent};
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.prevEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    }
+  });
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey:["events", {id: params.id}],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id})
+  });
+
+  function handleSubmit(formData) {
+    mutate({id: params.id, event: formData});
+    navigate("../")
+  }
 
   function handleClose() {
     navigate('../');
   }
 
+  let content;
+
+  if(isPending){
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if(isMutationPending){
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if(isError){
+    content = <>
+      <ErrorBlock title="Failed to load the event" message={error.info?.message || "Failed to load the event, please try after sometime."}/>
+      <div className='"form-actions'>
+        <Link to="../" className='button'>Okay</Link>
+      </div>
+    </>
+  }
+
+  if(data){
+    content = <EventForm inputData={data} onSubmit={handleSubmit}>
+    <Link to="../" className="button-text">
+      Cancel
+    </Link>
+    <button type="submit" className="button">
+      Update
+    </button>
+  </EventForm>
+  }
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
